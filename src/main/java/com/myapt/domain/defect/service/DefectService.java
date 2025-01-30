ac02d26a6ebcedcb06a2791ae2713848b097de3c
Junho Yu <ryuwon@gnu.ac.kr>
2025-01-30 17:12:03 +0900

    ✨ Feat[#6]: defect service 추가

---
+++ src/main/java/com/myapt/domain/defect/service/DefectService.java
@@ -0,0 +1,12 @@
+package com.myapt.domain.defect.service;
+
+import java.util.List;
+
+import com.myapt.domain.defect.dto.DefectInfoResponse;
+import com.myapt.domain.defect.dto.DefectMainResponse;
+
+public interface DefectService {
+	// MainResponseDto getDefectLocs();
+	List<DefectMainResponse> getDefectMain();
+	DefectInfoResponse getInfoDefectApt(Long id);
+}
---
+++ src/main/java/com/myapt/domain/defect/service/DefectServiceImpl.java
@@ -0,0 +1,68 @@
+package com.myapt.domain.defect.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.myapt.domain.apt.entity.Apts;
+import com.myapt.domain.apt.repository.AptRepository;
+import com.myapt.domain.defect.dto.DefectAptInfo;
+import com.myapt.domain.defect.dto.DefectBuildInfo;
+import com.myapt.domain.defect.dto.DefectInfoResponse;
+import com.myapt.domain.defect.dto.DefectMainResponse;
+import com.myapt.domain.defect.dto.DefectSplmnInfo;
+import com.myapt.domain.defect.entity.DefectApts;
+import com.myapt.domain.defect.exception.AptNotFoundException;
+import com.myapt.domain.defect.repository.DefectRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class DefectServiceImpl implements DefectService {
+	private final DefectRepository defectRepository;
+	private final AptRepository aptRepository;
+
+	@Override
+	@Transactional
+	public List<DefectMainResponse> getDefectMain() {
+		// 최근 뉴스 3개 들고오는 내용.
+		return null;
+	}
+
+	@Transactional
+	public DefectInfoResponse getInfoDefectApt(Long id) {
+		DefectApts defectApt = defectRepository.findById(id)
+			.orElseThrow(AptNotFoundException::new); // id에 해당하는 defectApt가 없으면 AptNotFoundException 발생
+
+		Apts apt = defectApt.getApts();
+		if (apt == null) {
+			throw new AptNotFoundException(); // apt가 null일 경우 예외 처리
+		}
+
+		// 건물 정보 생성
+		DefectAptInfo aptInfo = DefectAptInfo.of(
+			apt.getAptNm(),
+			apt.getRdnmadr(),
+			defectApt.getZipcode(),
+			defectApt.getDesgnr(),
+			apt.getCnstEntrprsNm(),
+			defectApt.getSprvsr()
+		);
+
+		// 건물 구조, 부실 사유 생성
+		DefectBuildInfo buildInfo = DefectBuildInfo.of(
+			apt.getBuldStru(),
+			defectApt.getDefectType()
+		);
+
+		// 보강 상태, 보강 내용 생성
+		DefectSplmnInfo splmnInfo = DefectSplmnInfo.of(
+			defectApt.getReinfStatus(),
+			defectApt.getReinfContent()
+		);
+
+		return DefectInfoResponse.of(aptInfo, buildInfo, splmnInfo);
+	}
+}
