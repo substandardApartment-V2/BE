42f0cd933e162e578d61bbe7c02d709d633f62d2
Seongbin Hong <beenie589@naver.com>
2025-01-30 21:12:20 +0900

    :sparkles: Feat[#15]: getApartmentInfoDetail 메서드 추가

--- src/main/java/com/myapt/domain/apt/service/AptService.java
+++ src/main/java/com/myapt/domain/apt/service/AptService.java
@@ -1,7 +1,9 @@
 package com.myapt.domain.apt.service;
 
+import com.myapt.domain.apt.dto.AptInfoDetail;
 import com.myapt.domain.apt.dto.AptInfo;
 
 public interface AptService {
     AptInfo getApartmentInfo(String aptsId);
+    AptInfoDetail getApartmentInfoDetail(String detailAptsId);
 }
