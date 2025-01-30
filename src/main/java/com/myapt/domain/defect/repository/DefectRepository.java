679f77c33015a370357ee4dd593c1719dd1e7165
Junho Yu <ryuwon@gnu.ac.kr>
2025-01-30 16:03:02 +0900

    ✨ Feat[#6]: 아파트 및 부실아파트 Repository 추가

---
+++ src/main/java/com/myapt/domain/defect/repository/DefectRepository.java
@@ -0,0 +1,11 @@
+package com.myapt.domain.defect.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.myapt.domain.defect.entity.DefectApts;
+
+@Repository
+public interface DefectRepository extends JpaRepository<DefectApts, Long> {
+}
+
