9376367f2d0a125bd8e64c6ab377801e64678c37
Junho Yu <ryuwon@gnu.ac.kr>
2025-02-14 20:40:02 +0900

    🐛 Fix[#30]: isDefect null 방지를 위한 타입변경

--- src/main/java/com/myapt/domain/apt/entity/Apts.java
+++ src/main/java/com/myapt/domain/apt/entity/Apts.java
@@ -47,7 +47,8 @@ public class Apts {
 
 	private String buldStru; // 건물구조
 
-	private Boolean isDefect; // 하자여부
+	@Column(name = "is_defect", nullable = false)
+	private boolean isDefect;
 
 	//연관관계
 	@OneToMany(mappedBy = "apts", cascade = CascadeType.ALL, orphanRemoval = true)
