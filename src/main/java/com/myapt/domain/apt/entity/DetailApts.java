2f5111c576e1c5965bb86e6b22fd12c58688221c
Jayden <beenie589@naver.com>
2025-02-15 16:57:18 +0900

    :bug: Feat[#11]  DetailApts가 비어있는 경우, 기본값을 반환하도록 AptServiceImpl의 getApartmentInfo 코드 수정

--- src/main/java/com/myapt/domain/apt/entity/DetailApts.java
+++ src/main/java/com/myapt/domain/apt/entity/DetailApts.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 @Table(name = "DetailApts")
 public class DetailApts {
 
