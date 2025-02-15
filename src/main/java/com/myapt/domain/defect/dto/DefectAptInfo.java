744cac252a5040261760883589d2dccbc392ccc6
Junho Yu <ryuwon@gnu.ac.kr>
2025-02-15 16:58:37 +0900

    ✨ Feat[#6]: 부실아파트 조회 API 응답 변경

--- src/main/java/com/myapt/domain/defect/dto/DefectAptInfo.java
+++ src/main/java/com/myapt/domain/defect/dto/DefectAptInfo.java
@@ -6,19 +6,13 @@ import lombok.Builder;
 public record DefectAptInfo(
 	String name, //아파트 명
 	String address, // 주소
-	String zipCode, // 우편번호
-	String desgnr, // 설계사
-	String cnstEntrprs, // 시공사
-	String sprvsr // 감리사
+	String zipCode // 우편번호
 ) {
-	public static DefectAptInfo of(String name, String address, String zipCode, String desgnr, String cnstEntrprs, String sprvsr) {
+	public static DefectAptInfo of(String name, String address, String zipCode) {
 		return DefectAptInfo.builder()
 			.name(name)
 			.address(address)
 			.zipCode(zipCode)
-			.desgnr(desgnr)
-			.cnstEntrprs(cnstEntrprs)
-			.sprvsr(sprvsr)
 			.build();
 	}
 }
--- src/main/java/com/myapt/domain/defect/dto/DefectInfoResponse.java
+++ src/main/java/com/myapt/domain/defect/dto/DefectInfoResponse.java
@@ -5,13 +5,15 @@ import lombok.Builder;
 @Builder
 public record DefectInfoResponse (
 	DefectAptInfo aptInfo,
+	DefectBasicInfo basicInfo,
 	DefectBuildInfo buildInfo,
 	DefectSplmnInfo splmnInfo
 ) {
 	public static DefectInfoResponse of(
-		DefectAptInfo aptInfo, DefectBuildInfo buildInfo, DefectSplmnInfo splmnInfo) {
+		DefectAptInfo aptInfo, DefectBasicInfo basicInfo, DefectBuildInfo buildInfo, DefectSplmnInfo splmnInfo) {
 		return DefectInfoResponse.builder()
 			.aptInfo(aptInfo)
+			.basicInfo(basicInfo)
 			.buildInfo(buildInfo)
 			.splmnInfo(splmnInfo)
 			.build();
