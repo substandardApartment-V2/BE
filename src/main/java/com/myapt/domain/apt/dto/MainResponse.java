a299eb0f07061181558cb86349d80d5d6b71d9f2
Jayden <beenie589@naver.com>
2025-02-18 07:44:13 +0900

    :sparkles: Feat[#20]: MainResponse DTO 수정

--- src/main/java/com/myapt/domain/apt/dto/MainResponse.java
+++ src/main/java/com/myapt/domain/apt/dto/MainResponse.java
@@ -1,21 +1,20 @@
 package com.myapt.domain.apt.dto;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Builder;
 
+@JsonPropertyOrder({"aptAvgPrice", "aptCount", "plannedAptCount"})
 @Builder
 public record MainResponse(
-	Long aptAvgPrice,
-	Long aptCount,
-	Long plannedAptCount,
-	LowestMgmtFeeAptInfo lowestMgmtFeeAptInfo
-
+		Long aptAvgPrice,
+		Long aptCount,
+		Long plannedAptCount
 ) {
-	public static MainResponse of(Long aptAvgPrice, Long aptCount, Long plannedAptCount, LowestMgmtFeeAptInfo lowestMgmtFeeAptInfo) {
+	public static MainResponse of(Long aptAvgPrice, Long aptCount, Long plannedAptCount) {
 		return MainResponse.builder()
-			.aptAvgPrice(aptAvgPrice)
-			.aptCount(aptCount)
-			.plannedAptCount(plannedAptCount)
-			.lowestMgmtFeeAptInfo(lowestMgmtFeeAptInfo)
-			.build();
+				.aptAvgPrice(aptAvgPrice)
+				.aptCount(aptCount)
+				.plannedAptCount(plannedAptCount)
+				.build();
 	}
 }
