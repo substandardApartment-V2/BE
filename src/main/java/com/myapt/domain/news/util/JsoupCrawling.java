df9b8d2e4f8dd1299bca545bded7a04a581b4c06
KJH <fighter0628@gmail.com>
2025-02-22 12:55:30 +0900

    :sparkles: Feat[#9] JsoupCrawling 개선 - 이미지,본문,제목 추출 방식 변경 및 플랫폼 정보 추가

--- src/main/java/com/myapt/domain/news/util/JsoupCrawling.java
+++ src/main/java/com/myapt/domain/news/util/JsoupCrawling.java
@@ -5,13 +5,13 @@ import java.util.Optional;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-/*
-connection을 생성하여 Elements를 반환
-IOException이 발생할 경우 Optional.empty()를 반환하여 NullPointerException을 방지
- */
 public class JsoupCrawling {
+	/*
+	connection을 생성하여 Document를 반환
+	 */
 	private Document getConnection(String url) throws IOException {
 		return Jsoup.connect(url).get();
 	}
@@ -39,12 +39,22 @@ public class JsoupCrawling {
 
 	/*
 	html 문서에서 이미지를 추출하는 메서드
-	id가 contents인 태그 안에 있는 img 태그의 속성 data-src의 값을 가져옴
+	meta 태그 중 property가 og:image인 태그의 content 속성 값을 가져옴
 	해당되는 값이 없는 경우 null 반환
 	 */
 	public String getImageUrl(Document document) {
-		String imageUrl = document.select("#contents img").attr("data-src");
-		return imageUrl.isBlank() ? null : imageUrl;
+		Element image = document.selectFirst("meta[property=og:image]");
+		return (image != null) ? image.attr("content") : null;
+	}
+
+	/*
+		html 문서에서 플랫폼 정보를 추출하는 메서드
+		meta 태그 중 name이 twitter:creator인 태그의 content 속성 값을 가져옴
+		해당 태그가 없으면 null 반환
+	 */
+	public String getPlatform(Document document) {
+		Element platform = document.selectFirst("meta[name=twitter:creator]");
+		return (platform != null) ? platform.attr("content") : null;
 	}
 
 	/*
@@ -52,10 +62,27 @@ public class JsoupCrawling {
 	본문 내용이 최대 길이보다 길면, 나머지 내용은 생략
 	 */
 	public String getContent(Document document, Integer maxLength) {
-		String text = document.text();
+		Element content = document.selectFirst("#newsct_article");
+		if (content == null) {
+			return null;
+		}
+		String text = content.text();
 		if (text.length() > maxLength) {
 			text = text.substring(0, maxLength - 3) + "...";
 		}
 		return text;
+	}
+
+	/*
+	html 문서에서 제목을 추출하는 메서드
+	meta 태그 중 property가 og:title인 태그의 content 속성 값을 가져옴
+	해당되는 값이 없는 경우 null 반환
+	 */
+	public String getTitle(Document document) {
+		Element title = document.selectFirst("meta[property=og:title]");
+		if (title == null) {
+			return null;
+		}
+		return title.attr("content");
 	}
 }
