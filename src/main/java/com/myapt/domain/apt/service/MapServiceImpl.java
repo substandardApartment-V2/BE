66fd58a68ec5fa12aa740eff61735b82eb3218e1
Junho Yu <ryuwon@gnu.ac.kr>
2025-03-07 00:13:27 +0900

    ✨ Feat[#58]: 아파트 주소 불러오기 로직 추가

--- src/main/java/com/myapt/domain/apt/service/MapServiceImpl.java
+++ src/main/java/com/myapt/domain/apt/service/MapServiceImpl.java
@@ -32,6 +32,7 @@ public class MapServiceImpl implements MapService {
 				.map(building -> MapMarkerInfo.of(
 					building.getId(),
 					building.getAptNm(),
+					building.getRdnmadr(),
 					building.getLa(),
 					building.getLo()))
 				.collect(Collectors.toList());
@@ -41,6 +42,7 @@ public class MapServiceImpl implements MapService {
 				.map(building -> MapMarkerInfo.of(
 					building.getId(),
 					building.getAptNm(),
+					building.getRdnmadr(),
 					building.getLa(),
 					building.getLo()))
 				.collect(Collectors.toList());
