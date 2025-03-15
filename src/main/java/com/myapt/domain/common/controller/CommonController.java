eba078c97dbdbbc74aa84985ca58ad28799d4f57
junoyou <ryuwon_@naver.com>
2025-03-15 18:26:16 +0900

    ✨ Feat[#68]: healthcheck를 위한 controller 선언 및 api 선언

---
+++ src/main/java/com/myapt/domain/common/controller/CommonController.java
@@ -0,0 +1,17 @@
+package com.myapt.domain.common.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/")
+public class CommonController {
+
+	@GetMapping("healthcheck")
+	public String healthCheck() {
+		return "OK";
+	}
+}
