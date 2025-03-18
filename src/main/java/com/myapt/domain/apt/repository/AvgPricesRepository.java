b1c0f7b4942d9fd5e08b3061ac1a1af594772047
Jayden <beenie589@naver.com>
2025-03-18 16:51:27 +0900

    :recycle: Feat[#50]: AvgPricesRepository에서, findByYearAndMonth 메소드로 수정

--- src/main/java/com/myapt/domain/apt/repository/AvgPricesRepository.java
+++ src/main/java/com/myapt/domain/apt/repository/AvgPricesRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface AvgPricesRepository extends JpaRepository<AvgPrices, String> {
-    //getMainInfo 메소드에서 현재 시간을 기준으로 '월'을 가져와서, 해당 월에 해당하는 평균 매매가를 AvgPricesRepository를 통해 조회하고, 이를 aptAvgPrice에 대입하는 코드
-    Optional<AvgPrices> findByMonth(Long month);
-}
+public interface AvgPricesRepository extends JpaRepository<AvgPrices, Long> {
+    //현재 시간을 기준으로  '연', '월'을 가져와서, 해당 월에 해당하는 평균 매매가를 찾음
+    Optional<AvgPrices> findByYearAndMonth(Long year, Long month);}
