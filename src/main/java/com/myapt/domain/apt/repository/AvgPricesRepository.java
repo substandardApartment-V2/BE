b75969aa439edcd23822fb1f93b563314cdc1d54
Jayden <beenie589@naver.com>
2025-03-19 13:36:33 +0900

    :recycle: Feat[#50]: repository 리펙토링

--- src/main/java/com/myapt/domain/apt/repository/AvgPricesRepository.java
+++ src/main/java/com/myapt/domain/apt/repository/AvgPricesRepository.java
@@ -9,4 +9,8 @@ import java.util.Optional;
 @Repository
 public interface AvgPricesRepository extends JpaRepository<AvgPrices, Long> {
     //현재 시간을 기준으로  '연', '월'을 가져와서, 해당 월에 해당하는 평균 매매가를 찾음
-    Optional<AvgPrices> findByYearAndMonth(Long year, Long month);}
+    Optional<AvgPrices> findByYearAndMonth(Long year, Long month);
+
+    //최신 데이터 조회 -> 해시값
+    Optional<AvgPrices> findTopByOrderByYearDescMonthDesc();
+}
--- src/main/java/com/myapt/domain/apt/repository/PlannedAptsRepository.java
+++ src/main/java/com/myapt/domain/apt/repository/PlannedAptsRepository.java
@@ -11,4 +11,7 @@ public interface PlannedAptsRepository extends JpaRepository<PlannedApts, Intege
 
     //연도와 월을 기준으로 검색
     Optional<PlannedApts> findByYearAndMonth(Long year, Long month);
+
+    // 최신 데이터 조회 -> 해시값 비교
+    Optional<PlannedApts> findTopByOrderByYearDescMonthDesc();
 }
