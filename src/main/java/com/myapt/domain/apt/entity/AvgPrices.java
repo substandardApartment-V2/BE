8980e715face32480e3a0070e75e394f8d4d57f1
Jayden <beenie589@naver.com>
2025-03-19 13:34:27 +0900

    :recycle: Feat[#50]: Entity 리펙토링

--- src/main/java/com/myapt/domain/apt/entity/AvgPrices.java
+++ src/main/java/com/myapt/domain/apt/entity/AvgPrices.java
@@ -2,26 +2,27 @@ package com.myapt.domain.apt.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
+@Table(indexes = @Index(columnList = "year,month"))
 public class AvgPrices {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "avg_price_id")
-    private Long id; // 평균매매가격 id -- 외부로부터 입력받음
+    private Long id;
 
-    @Column
-    private Long year; // 년
+    @Column(nullable = false)
+    private Long year;
 
-    @Column
-    private Long month; // 월
+    @Column(nullable = false)
+    private Long month;
 
-    @Column(name = "avg_price")
-    private Long avgPrice; // 평균 가격
+    @Column(name = "avg_price", nullable = false)
+    private Long avgPrice;
 
+    @Column(name = "date_list_hash", nullable = false)
+    private String dateListHash;
 }
