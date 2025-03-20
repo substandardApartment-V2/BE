fd23be899c63e31c67cef0b1a1fa3797ef3c6805
circlezero <wy129@naver.com>
2025-03-20 22:15:12 +0900

    Feat[#69] 공지사항 조회 예외 메세지 수정

--- src/main/java/com/myapt/domain/apt/dto/NoticeRequest.java
+++ src/main/java/com/myapt/domain/apt/dto/NoticeRequest.java
@@ -11,12 +11,15 @@ import lombok.Builder;
 @Builder
 public record NoticeRequest(
 		@NotNull(message = "{pagination.num.notNull}")
-		@Min(value = 1, message = "{pagination.num.min}")
-		@Max(value = 20, message = "{pagination.num.max}")
+//		@Min(value = 1, message = "{pagination.num.min}")
+//		@Max(value = 20, message = "{pagination.num.max}")
+		@Min(value = 1, message = "{pagination.num.invalid}")
+		@Max(value = 20, message = "{pagination.num.invalid}")
 		Integer num, // 가져올 요소 갯수
 
 		@NotNull(message = "{pagination.pages.notNull}")
-		@Min(value = 0, message = "{pagination.pages.min}")
+//		@Min(value = 0, message = "{pagination.pages.min}")\
+		@Min(value = 0, message = "{pagination.pages.invalid}")
 		Integer pages, // 페이지 번호
 
 		@AllowedSortValues(allowed = {"asc", "desc"})
