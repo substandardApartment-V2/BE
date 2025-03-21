f95b4feeb9d010c1f97da5b37bb13f43933a0eeb
KJH <fighter0628@gmail.com>
2025-03-22 02:29:02 +0900

    :bug: Fix[#72]: 문서 미수집 시 url에서 언론사 추출, NewsPlatform에 없는 언론사는 도메인 임시 처리

--- src/main/java/com/myapt/domain/news/dto/NewsSummaryDto.java
+++ src/main/java/com/myapt/domain/news/dto/NewsSummaryDto.java
@@ -1,15 +1,17 @@
 package com.myapt.domain.news.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
 import org.jsoup.nodes.Document;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonSetter;
+import com.myapt.domain.news.enums.NewsPlatform;
 import com.myapt.domain.news.util.JsoupCrawling;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-
-import java.time.OffsetDateTime;
 
 @Getter
 @Slf4j
@@ -19,6 +21,8 @@ public class NewsSummaryDto {
 	private String title;
 	@JsonSetter("link")
 	private String link;
+	@JsonSetter("originallink")
+	private String originalLink;
 	@JsonSetter("description")
 	private String description;
 	@JsonSetter("pubDate")
@@ -34,34 +38,35 @@ public class NewsSummaryDto {
 	*/
 	public NewsCrawlingResponse toNewsResponseDto(JsoupCrawling jsoupCrawling) {
 		// 뉴스 링크를 통해 html 문서 가져옴
-		Document document = jsoupCrawling.getDocument(link);
-		if (document == null) {
-			// document가 null인 경우 기본 값 또는 원본 데이터를 사용하여 응답 생성
-			return NewsCrawlingResponse.builder()
-				.platform("Naver")
-				.imageLink(link)
-				.title(title)
-				.link(link)
-				.description(description)
-				.pubDate(pubDate)
-				.build();
+		Optional<Document> document = jsoupCrawling.getDocument(link);
+
+		String platform;
+		String imageLink = null;
+		String newsTitle = title;
+		String newsContent = description;
+
+		if (document.isPresent()) {
+			// html 문서를 가져온 경우, 문서에서 언론사, 이미지, 제목, 내용을 가져옵니다
+			platform = jsoupCrawling.getPlatform(document.get());
+			imageLink = jsoupCrawling.getImageUrl(document.get());
+			newsTitle = jsoupCrawling.getTitle(document.get());
+			newsContent = jsoupCrawling.getContent(document.get(), 255);
+		} else {
+			// html 문서를 못 가져온 경우, 언론사는 네이버뉴스 url 에서 언론사별 고유한 값을 통해 추출합니다.
+			// https://n.news.naver.com/mnews/article/055/0001239771?sid=101 의 경우 055가 언론사 고유값에 해당함
+			Optional<NewsPlatform> newsPlatform = NewsPlatform.fromCode(link.split("/")[5]);
+			// 만약 NewsPlatform에 없는 값이라면, 언론사 사이트의 도메인을 가져와 임시로 platform에 저장하고
+			// 코드 수정을 통해 NewsPlatform에 해당 언론사를 추가합니다
+			platform = newsPlatform.map(NewsPlatform::getName)
+				.orElseGet(() -> originalLink.split("/")[2]);
 		}
 
-		// html 문서에서 플랫폼 추출
-		String extractedPlatform = jsoupCrawling.getPlatform(document);
-		// html 문서에서 이미지 url 추출
-		String extractedImageLink = jsoupCrawling.getImageUrl(document);
-		// html 문서에서 제목 추출
-		String extractedTitle = jsoupCrawling.getTitle(document);
-		// html 문서에서 본문 추출(최대 길이 설정 가능)
-		String extractedContent = jsoupCrawling.getContent(document, 255);
-
 		return NewsCrawlingResponse.builder()
-			.platform((extractedPlatform != null) ? extractedPlatform : "Naver")
-			.imageLink((extractedImageLink != null) ? extractedImageLink : link)
-			.title((extractedTitle != null) ? extractedTitle : title)
+			.platform(platform)
+			.imageLink(imageLink)
+			.title(newsTitle)
 			.link(link)
-			.description((extractedContent != null) ? extractedContent : description)
+			.description(newsContent)
 			.pubDate(pubDate)
 			.build();
 	}
